b8e691f296e49bc74fcf7c96567102b1db3efa20
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-07-24 03:01:41 +0200

    add sentry-debug url

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
+
+
 urlpatterns = [
     path('', include('layout.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
